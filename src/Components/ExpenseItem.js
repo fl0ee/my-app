c6201666cff04281a6023bc9a1b4382b8935119a
fl0ee <123451037+fl0ee@users.noreply.github.com>
2023-12-19 15:47:58 +0100

    1

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,22 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const Day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const Year = props.date.getFullYear();
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{Day} </div>
+        <div>{Year}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
